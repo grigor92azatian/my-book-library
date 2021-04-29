066cd656fcbe47c845e72f3a9f1b88ca2c33ed93
Grigor Azatian <grigorazatian@yahoo.com>
2021-04-29 12:42:04 -0700

    removed unused code that was already commented out

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ function Book(title, author, pages, read){                //Book object construc
     this.author = author;
     this.numPages = pages;
     this.read = read;
-    // this.displayString = function(){
-    //     //return `Title: ${this.title}, Author: ${this.author}, Number of Pages: ${this.numPages}, Read: ${this.read?"Yes":"No"}`;
-    //     return `<h2>Title: ${this.title}</h2><h3>Author: ${this.author}</h3><h4>Pages: ${this.numPages}</h4><h5>Read? ${this.read?"Yes":"No"}</h5>`;
-    // }
 }
 
 //take user input, make a book object, add book object to obj array
@@ -85,11 +81,6 @@ function displayBookToPage(bookObj){   //
 
 let addBookButton = document.querySelector("#addBook");
 let bookForm = document.querySelector("#newBookForm");
-// addBookButton.addEventListener("click",function(){
-//     let newBook = getUserInput();
-//     addBookToLibrary(newBook);
-//     displayBookToPage(newBook);
-// });
 
 addBookButton.addEventListener("click", function(event){
     event.preventDefault();
@@ -98,67 +89,3 @@ addBookButton.addEventListener("click", function(event){
     displayBookToPage(newBook);
     bookForm.reset();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function displayLibrary(){              //use this to add initial set of books to library
-//     let libraryDisplay = document.querySelector(".library-display");
-//     myLibrary.forEach(element => {
-//         let book = document.createElement("li");
-//         book.style.cssText = "color: black; list-style-type: none;"+
-//                                 "display: flex; flex-direction: column; align-items:center;"+
-//                                 "justify-content: space-between; width:20%; margin:10px; height: 150px;"+
-//                                 "background-image:url('https://cdn4.iconfinder.com/data/icons/small-n-flat/24/book-512.png');"+
-//                                 "background-size:200px 150px;"
-//         //book.innerText = element.displayString();
-//         book.innerHTML = element.displayString();
-//         libraryDisplay.appendChild(book);
-//     });
-// }
-
-// function addOneBook(){              //use this to add additional (user input) books to library
-
-// }
-
-
-
-
-
-
-// function addNewBookToLibrary(){         //get info for new book with this
-//     let title = prompt("Title?");
-//     let author = prompt("Author?");
-//     let pages = prompt("How many pages?");
-//     let read = prompt("Have you read this book?");
-//     let newBook = new Book(title, author, pages, read);
-//     myLibrary.push(newBook);
-//     displayLibrary();
-// }
-
-
-
